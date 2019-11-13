11bfca48b79cef4429d1851ecb7150bbc56c144d
wangtao <wangtao090620@gmail.com>
2019-11-13 19:20:22 +0800

    添加注释

--- wangtao/0001.py
+++ wangtao/0001.py
@@ -12,11 +12,11 @@
 class Solution:
     def twoSum(self, nums: list, target: int) -> list:
         hash_map = dict()
-        for i, x in enumerate(nums):  # 角标和对应的值
+        for i, x in enumerate(nums):  # 脚标和对应的值
             if (target - x) in hash_map:
                 return [i, hash_map.get(target - x)]
 
-            hash_map[x] = i
+            hash_map[x] = i  # 将新的值存储起来
 
 
 if __name__ == '__main__':
