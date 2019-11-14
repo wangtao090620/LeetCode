98b709b75268d15c5d62bc7ecf8fb9627bcf0780
wangtao <wangtao090620@gmail.com>
2019-11-14 20:20:15 +0800

    leetcode第7题

---
+++ wangtao/0007.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+# -*- encoding: utf-8 -*-
+
+# @Author  :   wangtao
+
+# @Contact :   wangtao090620@gmail.com
+
+# @Time    :   2019-11-14 19:43
+
+# 给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
+#
+# 示例 1:
+#
+# 输入: 123
+# 输出: 321
+#  示例 2:
+#
+# 输入: -123
+# 输出: -321
+# 示例 3:
+#
+# 输入: 120
+# 输出: 21
+
+
+class Solution:
+    def reverse(self, x: int) -> int:
+        s = (x > 0) - (x < 0)
+
+        r = int(str(x * s)[::-1])
+
+        return r * s * (r < 2 ** 31)
+
+
+if __name__ == '__main__':  # True 1，False 0
+
+    clazz = Solution()
+    print(clazz.reverse(210))
+
+
