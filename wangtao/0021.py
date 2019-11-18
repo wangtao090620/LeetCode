3ffda0419f444b39ad35f99b54cab4fb935dd1c2
wangtao <wangtao090620@gmail.com>
2019-11-18 11:28:10 +0800

    leetcode第21题

---
+++ wangtao/0021.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+# -*- encoding: utf-8 -*-
+
+# @Author  :   wangtao
+
+# @Contact :   wangtao090620@gmail.com
+
+# @Time    :   2019-11-18 11:17
+
+# 将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+#
+# 示例：
+#
+# 输入：1->2->4, 1->3->4
+# 输出：1->1->2->3->4->4
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode):
+
+        if not l1 or not l2:
+            return l1 or l2
+        if l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
+
+
+if __name__ == '__main__':
+    pass
