74c384948b81c78cec9d3ecaf2e8eee9c83d239d
wangtao <wangtao090620@gmail.com>
2019-11-18 18:03:06 +0800

    leetcode第23题

---
+++ wangtao/0023.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+
+# -*- encoding: utf-8 -*-
+
+# @Author  :   wangtao
+
+# @Contact :   wangtao090620@gmail.com
+
+# @Time    :   2019-11-18 14:29
+
+
+# 合并 k 个排序链表，返回合并后的排序链表。请分析和描述算法的复杂度。
+#
+# 示例:
+#
+# 输入:
+# [
+#   1->4->5,
+#   1->3->4,
+#   2->6
+# ]
+# 输出: 1->1->2->3->4->4->5->6
+
+import queue
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+
+        if not lists:
+            return None
+        amount = len(lists)
+        interval = 1
+        while interval < amount:
+            for i in range(0, amount - interval, interval * 2):
+                lists[i] = self.merge2Lists(lists[i], lists[i + interval])
+            interval *= 2
+        return lists[0] if amount > 0 else lists
+
+    def merge2Lists(self, l1, l2):
+        head = point = ListNode(0)
+        while l1 and l2:
+            if l1.val <= l2.val:
+                point.next = l1
+                l1 = l1.next
+            else:
+                point.next = l2
+                l2 = l1
+                l1 = point.next.next
+            point = point.next
+        if not l1:
+            point.next = l2
+        else:
+            point.next = l1
+        return head.next
+
+
+if __name__ == '__main__':
+    pass
