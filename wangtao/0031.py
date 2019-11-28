0d96df209fcea2dd003dd2f902c568067c931fdf
wangtao <wangtao090620@gmail.com>
2019-11-28 20:42:15 +0800

    leetcode第31题

---
+++ wangtao/0031.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+# -*- encoding: utf-8 -*-
+
+# @Author  :   wangtao
+
+# @Contact :   wangtao090620@gmail.com
+
+# @Time    :   2019-11-19 22:56
+
+# 实现获取下一个排列的函数，算法需要将给定数字序列重新排列成字典序中下一个更大的排列。
+# 如果不存在下一个更大的排列，则将数字重新排列成最小的排列（即升序排列）。
+# 必须原地修改，只允许使用额外常数空间。
+# 以下是一些例子，输入位于左侧列，其相应输出位于右侧列。
+#
+# 1,2,3 → 1,3,2
+# 3,2,1 → 1,2,3
+# 1,1,5 → 1,5,1
+
+
+class Solution:
+    def nextPermutation(self, nums):
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+
+        i = j = len(nums) - 1
+
+        while i > 0 and nums[i - 1] >= nums[i]:
+            i -= 1
+
+        if i == 0:
+            nums.reverse()
+            return
+
+        k = i - 1
+        while nums[j] <= nums[k]:
+            j -= 1
+        nums[k], nums[j] = nums[j], nums[k]
+        l, r = k + 1, len(nums) - 1
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l += 1;r -= 1
+
+
+if __name__ == '__main__':
+    pass
