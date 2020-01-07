79c72d18c811d322bbddccdfff45615d6cc2159f
wangtao <wangtao090620@gmail.com>
2020-01-07 17:41:12 +0800

    leetcode 0283题

---
+++ wangtao/0283.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+# -*- encoding: utf-8 -*-
+
+# @Author  :   wangtao
+
+# @Contact :   wangtao090620@gmail.com
+
+# @Time    :   2020-01-07 17:35
+
+
+"""
+给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+
+示例:
+
+输入: [0,1,0,3,12]
+输出: [1,3,12,0,0]
+说明:
+
+必须在原数组上操作，不能拷贝额外的数组。
+尽量减少操作次数。
+
+"""
+from typing import List
+
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        zero = 0
+
+        for i in range(len(nums)):
+
+            if nums[i] != 0:
+                nums[zero], nums[i] = nums[i], nums[zero]
+                zero += 1
+
+
+if __name__ == '__main__':
+    pass
