15935af12b90501e1bdc652e4964bcee9c259d2f
wangtao <wangtao090620@gmail.com>
2020-01-13 19:17:00 +0800

    leetcode 0581题

---
+++ wangtao/0581.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+# -*- encoding: utf-8 -*-
+
+# @Author  :   wangtao
+
+# @Contact :   wangtao090620@gmail.com
+
+# @Time    :   2020-01-13 19:04
+
+
+"""
+给定一个整数数组，你需要寻找一个连续的子数组，如果对这个子数组进行升序排序，那么整个数组都会变为升序排序。
+
+你找到的子数组应是最短的，请输出它的长度。
+
+示例 1:
+
+输入: [2, 6, 4, 8, 10, 9, 15]
+输出: 5
+解释: 你只需要对 [6, 4, 8, 10, 9] 进行升序排序，那么整个表都会变为升序排序。
+说明 :
+
+输入的数组长度范围在 [1, 10,000]。
+输入的数组可能包含重复元素 ，所以升序的意思是<=。
+
+"""
+from typing import List
+
+
+class Solution:
+    def findUnsortedSubarray(self, nums: List[int]) -> int:
+        res = [i for i, (a, b) in enumerate(zip(nums, sorted(nums))) if a != b]
+
+        return 0 if not res else res[-1] - res[0] + 1
+
+
+if __name__ == '__main__':
+    pass
