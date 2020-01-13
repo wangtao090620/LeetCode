675c89b675f4ccbcccf7c314c3958c3af6e2b69c
wangtao <wangtao090620@gmail.com>
2020-01-13 19:26:27 +0800

    leetcode 0617题

---
+++ wangtao/0617.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+# -*- encoding: utf-8 -*-
+
+# @Author  :   wangtao
+
+# @Contact :   wangtao090620@gmail.com
+
+# @Time    :   2020-01-13 19:17
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def mergeTrees(self, t1: TreeNode, t2: TreeNode) -> TreeNode:
+        if t1 and t2:
+            root = TreeNode(t1.val + t2.val)
+            root.left = self.mergeTrees(t1.left, t2.left)
+            root.right = self.mergeTrees(t1.right, t2.right)
+            return root
+        else:
+            return t1 or t2
+
+
+if __name__ == '__main__':
+    pass
