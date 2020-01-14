5594cb09b3f40a6a8e61314fa503e238387adec2
wangtao <wangtao090620@gmail.com>
2020-01-14 15:29:52 +0800

    leetcode 0621题公式

--- wangtao/0621.py
+++ wangtao/0621.py
@@ -41,6 +41,34 @@ A -> B -> (单位时间) -> A -> B -> (单位时间) -> A -> B
 https://leetcode.com/problems/task-scheduler/discuss/104500/Java-O(n)-time-O(1)-space-1-pass-no-sorting-solution-with-detailed-explanation
 
 """
+
+"""
+public int leastInterval(char[] tasks, int n) {
+        int[] counter = new int[26];
+        int max = 0;
+        int maxCount = 0;
+        for(char task : tasks) {
+            counter[task - 'A']++;
+            if(max == counter[task - 'A']) {
+                maxCount++;
+            }
+            else if(max < counter[task - 'A']) {
+                max = counter[task - 'A'];
+                maxCount = 1;
+            }
+        }
+        
+        int partCount = max - 1;
+        int partLength = n - (maxCount - 1);
+        int emptySlots = partCount * partLength;
+        int availableTasks = tasks.length - max * maxCount;
+        int idles = Math.max(0, emptySlots - availableTasks);
+        
+        return tasks.length + idles;
+    }
+"""
+
+# （m-1）*(n+1)+count
 from typing import List
 
 from collections import Counter
