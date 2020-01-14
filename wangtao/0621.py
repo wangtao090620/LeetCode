4c64a6ae14ae936f863e2cdafc2b05d479f81e95
wangtao <wangtao090620@gmail.com>
2020-01-14 11:51:20 +0800

    leetcode 0621题

---
+++ wangtao/0621.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+# -*- encoding: utf-8 -*-
+
+# @Author  :   wangtao
+
+# @Contact :   wangtao090620@gmail.com
+
+# @Time    :   2020-01-13 19:27
+
+"""
+给定一个用字符数组表示的 CPU 需要执行的任务列表。其中包含使用大写的 A - Z 字母表示的26 种不同种类的任务。任务可以以任意顺序执行，并且每个任务都可以在 1 个单位时间内执行完。CPU 在任何一个单位时间内都可以执行一个任务，或者在待命状态。
+
+然而，两个相同种类的任务之间必须有长度为 n 的冷却时间，因此至少有连续 n 个单位时间内 CPU 在执行不同的任务，或者在待命状态。
+
+你需要计算完成所有任务所需要的最短时间。
+
+示例 1：
+
+输入: tasks = ["A","A","A","B","B","B"], n = 2
+输出: 8
+执行顺序: A -> B -> (待命) -> A -> B -> (待命) -> A -> B.
+注：
+
+任务的总个数为 [1, 10000]。
+n 的取值范围为 [0, 100]。
+
+"""
+
+"""
+因为相同任务必须要有时间片为 n 的间隔，所以我们先把出现次数最多的任务 A 安排上（当然你也可以选择任务 B）。
+
+例子中 n = 2，那么任意两个任务 A 之间都必须间隔 2 个单位的时间：
+
+A -> (单位时间) -> (单位时间) -> A -> (单位时间) -> (单位时间) -> A
+中间间隔的单位时间可以用来安排别的任务，也可以处于“待命”状态。当然，为了使总任务时间最短，我们要尽可能地把单位时间分配给其他任务。现在把任务 B 安排上：
+
+A -> B -> (单位时间) -> A -> B -> (单位时间) -> A -> B
+
+
+https://leetcode.com/problems/task-scheduler/discuss/104500/Java-O(n)-time-O(1)-space-1-pass-no-sorting-solution-with-detailed-explanation
+
+"""
+from typing import List
+
+from collections import Counter
+
+
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        task_dict = Counter(tasks)
+
+        # 最多任务出现的次数
+        maxCount = max(task_dict.values())
+        # 最多任务共有多少个
+        count = 0
+        for val in task_dict.values():
+            if val == maxCount:
+                count += 1
+
+        return max((maxCount - 1) * (n + 1) + count, len(tasks))
+
+
+if __name__ == '__main__':
+    pass
