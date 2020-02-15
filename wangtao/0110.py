d15085e110863a0e2b7f53e5f804adb1d921d591
wangtao <wangtao090620@gmail.com>
2020-02-15 15:27:58 +0800

    feat{LeetCode}:LeetCode 0110题

---
+++ wangtao/0110.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+# -*- encoding: utf-8 -*-
+
+# @Author  :   wangtao
+
+# @Contact :   wangtao090620@gmail.com
+
+# @Time    :   2020-02-15 15:26
+
+
+"""
+
+给定一个二叉树，判断它是否是高度平衡的二叉树。
+
+本题中，一棵高度平衡二叉树定义为：
+
+一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过1。
+
+示例 1:
+
+给定二叉树 [3,9,20,null,null,15,7]
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+返回 true 。
+
+示例 2:
+
+给定二叉树 [1,2,2,3,3,null,null,4,4]
+
+       1
+      / \
+     2   2
+    / \
+   3   3
+  / \
+ 4   4
+返回 false 。
+
+"""
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def isBalanced(self, root: TreeNode) -> bool:
+        return self.check(root) != -1
+
+    def check(self, root):
+        if not root:
+            return 0
+
+        l = self.check(root.left)
+        r = self.check(root.right)
+
+        if abs(l - r) or l == -1 or r == -1:
+            return -1
+        return 1 + max(l, r)
+
+
+if __name__ == '__main__':
+    pass
